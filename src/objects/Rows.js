8582c47596273b9fd84b0c5adf161a0c1d9778de
Daniel Archibald <47116316+pagodingo@users.noreply.github.com>
2023-02-19 18:03:18 -0500

    -

--- src/objects/Rows.js
+++ src/objects/Rows.js
@@ -6,7 +6,7 @@ const Rows = (rowNumber) => {
     
     for (let i = 0; i < length; ++i)
     {
-        row.appendChild(Cell(i + 1, rowNumber + 1)) // renders 0,0 by default- fixing later
+        row.appendChild(Cell(i + 1, rowNumber + 1));
     }
 return  row
 }
